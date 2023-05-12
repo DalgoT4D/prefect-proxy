7cf04ef13596d02335b3c5b0bdc09f600d9d6d7d
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-05-12 07:21:56 +0530

    black auto-formatting removed unused imports

--- tests.py
+++ tests.py
@@ -1,9 +1,31 @@
 import os
+import pytest
 from pydantic import ValidationError
 from main import delete_deployment
-from schemas import AirbyteConnectionBlockResponse, AirbyteConnectionCreate, AirbyteServerBlockResponse, AirbyteServerCreate, DbtCliProfile, DbtCoreBlockResponse, DbtCoreCreate, DbtProfileCreate, DeploymentCreate, FlowRunsResponse, PostDeploymentResponse, RunFlow
-from service import create_airbyte_connection_block, create_airbyte_server_block, create_dbt_core_block, delete_airbyte_connection_block, delete_airbyte_server_block, delete_dbt_core_block, get_airbyte_connection_block_id, get_airbyte_server_block_id, get_dbtcore_block_id, get_flow_runs_by_deployment_id, post_deployment, run_airbyte_connection_prefect_flow, run_dbtcore_prefect_flow
-import pytest
+from schemas import (
+    AirbyteConnectionBlockResponse,
+    AirbyteConnectionCreate,
+    AirbyteServerBlockResponse,
+    AirbyteServerCreate,
+    DbtCoreBlockResponse,
+    DbtCoreCreate,
+    DeploymentCreate,
+    FlowRunsResponse,
+    PostDeploymentResponse,
+)
+from service import (
+    create_airbyte_connection_block,
+    create_airbyte_server_block,
+    create_dbt_core_block,
+    delete_airbyte_connection_block,
+    delete_airbyte_server_block,
+    delete_dbt_core_block,
+    get_airbyte_connection_block_id,
+    get_airbyte_server_block_id,
+    get_dbtcore_block_id,
+    get_flow_runs_by_deployment_id,
+    post_deployment,
+)
 
 
 class TestAirbyteServer:
@@ -28,15 +50,16 @@ class TestAirbyteServer:
             TestAirbyteServer.block_id = res
         except ValidationError as e:
             raise ValueError(f"Response validation failed: {e.errors()}")
-        
+
     @pytest.mark.asyncio
     async def test_get_airbyte_server_block_id(self):
         try:
-            res = await get_airbyte_server_block_id(blockname='airbyte1')
+            res = await get_airbyte_server_block_id(blockname="airbyte1")
             AirbyteServerBlockResponse(block_id=res)
         except ValidationError as e:
             raise ValueError(f"Response validation failed: {e.errors()}")
-        
+
+
 class TestAirbyteConnection:
     block_id = None
 
@@ -45,7 +68,7 @@ class TestAirbyteConnection:
         payload = {
             "serverBlockName": "airbyte1",
             "connectionId": "6a791af6-eb58-11ed-a05b-0242ac120009",
-            "connectionBlockName": "blockkkkk"
+            "connectionBlockName": "block",
         }
         try:
             validated_payload = AirbyteConnectionCreate(**payload)
@@ -58,25 +81,24 @@ class TestAirbyteConnection:
             TestAirbyteConnection.block_id = res
         except ValidationError as e:
             raise ValueError(f"Response validation failed: {e.errors()}")
-        
+
     @pytest.mark.asyncio
     async def test_get_airbyte_connection_block_id(self):
         try:
-            res = await get_airbyte_connection_block_id(blockname='blockkkkk')
+            res = await get_airbyte_connection_block_id(blockname="blockkkkk")
             AirbyteServerBlockResponse(block_id=res)
         except ValidationError as e:
             raise ValueError(f"Response validation failed: {e.errors()}")
-        
+
     def test_delete_airbyte_connection_block(self):
         try:
             delete_airbyte_server_block(blockid=TestAirbyteServer.block_id)
             delete_airbyte_connection_block(blockid=TestAirbyteConnection.block_id)
         except ValidationError as e:
             raise ValueError(f"Response validation failed: {e.errors()}")
-    
+
 
 class TestDbtConnection:
-
     @pytest.mark.asyncio
     async def test_create_dbt_core_block(self):
         payload = {
@@ -84,22 +106,22 @@ class TestDbtConnection:
             "profile": {
                 "name": "shri_dbt",
                 "target": "dev",
-                "target_configs_schema": "dev"
+                "target_configs_schema": "dev",
             },
             "wtype": "postgres",
             "credentials": {
-                "host": os.getenv('DB_HOST'),
+                "host": os.getenv("DB_HOST"),
                 "port": 5432,
-                "database": os.getenv('DB_NAME'),
-                "username": os.getenv('DB_USER'),
+                "database": os.getenv("DB_NAME"),
+                "username": os.getenv("DB_USER"),
                 "ssl": False,
-                "password": os.getenv('DB_PASSWORD')
+                "password": os.getenv("DB_PASSWORD"),
             },
             "commands": ["dbt list"],
             "working_dir": "/tmp",
             "env": {"key": "value"},
             "profiles_dir": "/dir",
-            "project_dir": "/dir"
+            "project_dir": "/dir",
         }
         try:
             validated_payload = DbtCoreCreate(**payload)
@@ -116,11 +138,11 @@ class TestDbtConnection:
     @pytest.mark.asyncio
     async def test_get_dbtcore_block_id(self):
         try:
-            res = await get_dbtcore_block_id(blockname='test')
+            res = await get_dbtcore_block_id(blockname="test")
             DbtCoreBlockResponse(block_id=res)
         except ValidationError as e:
             raise ValueError(f"Response validation failed: {e.errors()}")
-        
+
     def test_delete_dbt_core_block(self):
         try:
             delete_dbt_core_block(block_id=TestDbtConnection.block_id)
@@ -137,7 +159,7 @@ class TestFlowDeployment:
             "org_slug": "test_org",
             "connection_blocks": ["blockkkkk", "block2"],
             "dbt_blocks": [],
-            "cron": "0 9 * * *"
+            "cron": "0 9 * * *",
         }
         try:
             validated_payload = DeploymentCreate(**payload)
@@ -146,9 +168,9 @@ class TestFlowDeployment:
 
         try:
             res = await post_deployment(validated_payload)
-            res['id'] = str(res['id'])
+            res["id"] = str(res["id"])
             PostDeploymentResponse(deployment=res)
-            TestFlowDeployment.deployment_id = res['id']
+            TestFlowDeployment.deployment_id = res["id"]
         except ValidationError as e:
             raise ValueError(f"Response validation failed: {e.errors()}")
 
@@ -161,11 +183,10 @@ class TestFlowDeployment:
             FlowRunsResponse(flow_runs=res)
         except Exception as e:
             raise ValueError(f"Test failed: {e}")
-        
+
     def test_delete_deployment(self):
         deployment_id = TestFlowDeployment.deployment_id
         try:
             delete_deployment(deployment_id)
         except Exception as e:
             raise ValueError(f"Test failed: {e}")
-        
