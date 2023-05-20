7920df495ce716dd479f1fd877aaed967f7d607d
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-05-20 09:52:42 +0530

    catch exceptions, log them, return to caller

--- flows.py
+++ flows.py
@@ -1,24 +1,34 @@
 """Reusable flows"""
 
+from fastapi import HTTPException
 from prefect import flow
 from prefect_airbyte.flows import run_connection_sync
 from prefect_airbyte import AirbyteConnection
 from prefect_dbt.cli.commands import DbtCoreOperation
 from schemas import RunFlow
+from logger import logger
 
 
 @flow
 def run_airbyte_connection_flow(payload: RunFlow):
     """Prefect flow to run airbyte connection"""
     airbyte_connection = AirbyteConnection.load(payload.blockName)
-    return run_connection_sync(airbyte_connection)
+    try:
+        return run_connection_sync(airbyte_connection)
+    except Exception as error:
+        logger.exception(error)
+        raise HTTPException(status_code=400, detail=str(error)) from error
 
 
 @flow
 def run_dbtcore_flow(payload: RunFlow):
     """Prefect flow to run dbt"""
     dbt_op = DbtCoreOperation.load(payload.blockName)
-    return dbt_op.run()
+    try:
+        return dbt_op.run()
+    except Exception as error:
+        logger.exception(error)
+        raise HTTPException(status_code=400, detail=str(error)) from error
 
 
 @flow
@@ -34,9 +44,17 @@ def deployment_schedule_flow(airbyte_blocks: list, dbt_blocks: list):
     # run airbyte blocks
     for block in airbyte_blocks:
         airbyte_connection = AirbyteConnection.load(block["blockName"])
-        run_connection_sync(airbyte_connection)
+        try:
+            return run_connection_sync(airbyte_connection)
+        except Exception as error:
+            logger.exception(error)
+            raise HTTPException(status_code=400, detail=str(error)) from error
 
     # run dbt blocks
     for block in dbt_blocks:
         dbt_op = DbtCoreOperation.load(block["blockName"])
-        dbt_op.run()
+        try:
+            return dbt_op.run()
+        except Exception as error:
+            logger.exception(error)
+            raise HTTPException(status_code=400, detail=str(error)) from error
