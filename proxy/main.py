ceca6c16a4251a82e34ea0552b3b8e044b4f3558
Ishankoradia <ikoradia@umich.edu>
2024-09-25 19:42:24 +0530

    cleanup

--- proxy/main.py
+++ proxy/main.py
@@ -54,7 +54,6 @@ from proxy.schemas import (
     DbtCliProfileBlockUpdate,
     RunAirbyteResetConnection,
     ScheduleFlowRunRequest,
-    DeleteFlowRunRequest,
 )
 from proxy.flows import run_airbyte_connection_flow
 
