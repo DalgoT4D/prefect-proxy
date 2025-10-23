cfb52416369211224f7217fbeb61b7f6e6a3e595
Ishankoradia <ikoradia@umich.edu>
2025-10-23 14:01:21 +0530

    revert

--- proxy/prefect_flows.py
+++ proxy/prefect_flows.py
@@ -87,7 +87,7 @@ def run_airbyte_conn_clear(payload: dict):
         )
         result = None
         if "streams" in payload and payload["streams"]:
-            result = clear_connection_streams(connection_block, [{"streamName": "sheet1", "streamNamespace": None}])
+            result = clear_connection_streams(connection_block, payload["streams"])
         else:
             result = clear_connection(connection_block)
 
