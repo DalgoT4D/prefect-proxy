c508754ca87c423743bb6fa3a6ed78d0d9e9f87d
Ishankoradia <ikoradia@umich.edu>
2026-05-27 15:40:31 +0530

    prefect upgrade to 3.6.24

--- kill.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#CELERY_WORKER_PID=`cat celeryworker.pid`
-#kill -9 "${CELERY_WORKER_PID}"
-
-ps aux | grep "prefect-proxy" | grep uvicorn | grep -v grep | awk '{print $2}' | xargs kill
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-GOOGLE_APPLICATION_CREDENTIALS=../../secrets/gcp.json
-uv run gunicorn proxy.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8500
